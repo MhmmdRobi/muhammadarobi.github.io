034765421fdbafd4a40f9895c42dddbabbb26c49
MhmmdRobi <muhamadarobii09@gmail.com>
2023-11-14 21:15:56 +0800

    first commit

---
+++ type.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings : ["adalah Mahasiwa Stitek Bontang", "Berumur 20 Tahun", "Tinggal di Kota Bontang"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
